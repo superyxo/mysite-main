915a3473446265ebf5c5cb34e787b0fae3edb320
lpgray <rayzy1991@gmail.com>
2013-06-22 16:44:57 +0800

    share static files with html-project

--- web-project/mysite/settings.py
+++ web-project/mysite/settings.py
@@ -85,7 +85,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ROOT_PATH + '/statics',
+    ROOT_PATH + '/html-project',
 )
 # List of finder classes that know how to find static files in
 # various locations.
