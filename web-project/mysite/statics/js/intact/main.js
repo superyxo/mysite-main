2ab5e2343cfb627c0e75572da9af9bf59b8475d6
lpgray <rayzy1991@gmail.com>
2013-06-28 22:31:13 +0800

    pre.js

--- web-project/mysite/statics/js/intact/main.js
+++ web-project/mysite/statics/js/intact/main.js
@@ -1,24 +1,3 @@
-var Utils = {}
-
-Utils.hasFeature = function(str){
-	return document.getElementsByTagName('html')[0].className.indexOf(str) > -1;
-}
-
-Utils.filterFileName = function(fullPath){
-	if (fullPath) {
-		var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
-		var filename = fullPath.substring(startIndex);
-		if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
-			filename = filename.substring(1);
-		}
-		return filename;
-	}
-}
-
-Utils.isMobile = function(){
-	return /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
-}
-
 // stop default link behavior
 $(document).on('click', '[href="#"],.disabled', function(e) {
 	e.preventDefault();
@@ -32,18 +11,10 @@ function resetOptPanel( margin ){
 	ctnLeft = ctn.offset().left;
 	panWidth = panel.width();
 	panel.css('right', ctnLeft - panWidth - margin);
-	
 }
 
 function toTop(){
 	$(window).scrollTop(0);
-}
-
-function beautifyBannerHome(){
-	if( !Utils.isMobile() ) {
-		var screeW = screen.width;
-		$('.banner').css('background-size',screeW);
-	}
 }
 
 function checkfixTop(){
@@ -82,10 +53,17 @@ function bindJQValiEngin() {
 	$('.j_vali_form').validationEngine();
 }
 
+function beautifyBannerHome(){
+	if( !Utils.isMobile() ) {
+		var screeW = screen.width;
+		$('.banner').css('background-size',screeW);
+		// console.log( document.getElementsByTagName('html')[0].className );
+	}
+}
 
 $(document).ready(function(){
-	resetOptPanel(20);
 	beautifyBannerHome();
+	resetOptPanel(20);
 	checkfixTop();
 	bindJQValiEngin();
 	window.prettyPrint && prettyPrint();
