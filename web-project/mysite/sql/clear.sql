bd367f75554752877068b46f617de09f3bcaa7b3
unknown <ray@.(none)>
2013-07-02 21:15:29 +0800

    remove sql

--- web-project/mysite/sql/clear.sql
+++
@@ -1,7 +0,0 @@
-delete from ms_articles_imgs;
-delete from ms_articles_tags;
-delete from ms_images;
-delete from ms_articles where id>1;
-delete from ms_trends;
-delete from ms_tags;
-delete from ms_comments;
