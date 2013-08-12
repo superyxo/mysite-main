727f06d207349c26d21791378eb42e9ac1a42f96
lpgray <rayzy1991@gmail.com>
2013-08-12 09:11:09 +0800

    fix banner bug.

--- web-project/mysite/statics/js/main/main.js
+++ web-project/mysite/statics/js/main/main.js
@@ -57,8 +57,8 @@ function beautifyBanner(){
 	if( !Utils.isMobile() ) {
 		var screeW = screen.width
 				, $banner = $('.banner')
-		$banner.css('background-size', screeW);
 		$banner.css('background', 'url(http://rayzy1991-media.stor.sinaapp.com/banner/banner-' + new Date().getDay() + '.jpg) center center no-repeat');
+	  $banner.css('background-size', screeW);
 	}
 }
 
